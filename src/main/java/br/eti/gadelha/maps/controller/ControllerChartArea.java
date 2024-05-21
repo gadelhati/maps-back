6431c1722c0051c90bd833eafc6b4daa5c61d06d
GadelhaTI <gadelha.ti@gmail.com>
2024-05-21 11:14:55 -0300

    bug: change object

--- src/main/java/br/eti/gadelha/maps/controller/ControllerChartArea.java
+++ src/main/java/br/eti/gadelha/maps/controller/ControllerChartArea.java
@@ -1,8 +1,8 @@
 package br.eti.gadelha.maps.controller;
 
-import br.eti.gadelha.maps.persistence.payload.request.DTORequestChart;
-import br.eti.gadelha.maps.persistence.payload.response.DTOResponseChart;
-import br.eti.gadelha.maps.service.ServiceChart;
+import br.eti.gadelha.maps.persistence.payload.request.DTORequestChartArea;
+import br.eti.gadelha.maps.persistence.payload.response.DTOResponseChartArea;
+import br.eti.gadelha.maps.service.ServiceChartArea;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -17,31 +17,31 @@ import java.net.URI;
 import java.util.UUID;
 
 @RestController @RequestMapping("/chart_area") @RequiredArgsConstructor
-public class ControllerChartArea implements ControllerInterface<DTOResponseChart, DTORequestChart> {
+public class ControllerChartArea implements ControllerInterface<DTOResponseChartArea, DTORequestChartArea> {
 
-    private final ServiceChart serviceChart;
+    private final ServiceChartArea serviceChartArea;
 
     @PostMapping("") @PreAuthorize("hasAnyRole('52c57a80-4e3b-4a41-a864-58d0cea25b14', '8652ec73-0a53-433c-93be-420f1d90c681')")
-    public ResponseEntity<DTOResponseChart> create(@RequestBody @Valid DTORequestChart created){
+    public ResponseEntity<DTOResponseChartArea> create(@RequestBody @Valid DTORequestChartArea created){
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/chart_area").toUriString());
-        return ResponseEntity.created(uri).body(serviceChart.create(created));
+        return ResponseEntity.created(uri).body(serviceChartArea.create(created));
     }
     @GetMapping("") @PreAuthorize("hasAnyRole('USER', '52c57a80-4e3b-4a41-a864-58d0cea25b14', '8652ec73-0a53-433c-93be-420f1d90c681')")
-    public ResponseEntity<Page<DTOResponseChart>> retrieve(@RequestParam(name="value", defaultValue = "", required = false) String value, Pageable pageable){
-        return ResponseEntity.ok().body(serviceChart.retrieve(pageable, value));
+    public ResponseEntity<Page<DTOResponseChartArea>> retrieve(@RequestParam(name="value", defaultValue = "", required = false) String value, Pageable pageable){
+        return ResponseEntity.ok().body(serviceChartArea.retrieve(pageable, value));
     }
     @PutMapping("") @PreAuthorize("hasAnyRole('52c57a80-4e3b-4a41-a864-58d0cea25b14', '8652ec73-0a53-433c-93be-420f1d90c681')")
-    public ResponseEntity<DTOResponseChart> update(@RequestBody @Valid DTORequestChart updated){
-        return ResponseEntity.accepted().body(serviceChart.update(updated.getId(), updated));
+    public ResponseEntity<DTOResponseChartArea> update(@RequestBody @Valid DTORequestChartArea updated){
+        return ResponseEntity.accepted().body(serviceChartArea.update(updated.getId(), updated));
     }
     @DeleteMapping("/{id}") @PreAuthorize("hasAnyRole('52c57a80-4e3b-4a41-a864-58d0cea25b14', '8652ec73-0a53-433c-93be-420f1d90c681')")
-    public ResponseEntity<DTOResponseChart> delete(@PathVariable UUID id){
-        return ResponseEntity.accepted().body(serviceChart.delete(id));
+    public ResponseEntity<DTOResponseChartArea> delete(@PathVariable UUID id){
+        return ResponseEntity.accepted().body(serviceChartArea.delete(id));
     }
     @DeleteMapping("") @PreAuthorize("hasAnyRole('52c57a80-4e3b-4a41-a864-58d0cea25b14', '8652ec73-0a53-433c-93be-420f1d90c681')")
     public ResponseEntity<HttpStatus> delete(){
         try {
-            serviceChart.delete();
+            serviceChartArea.delete();
             return new ResponseEntity<>(null, HttpStatus.ACCEPTED);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(HttpStatus.BAD_REQUEST);
