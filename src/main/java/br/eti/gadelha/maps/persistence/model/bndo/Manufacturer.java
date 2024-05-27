1b022626a4d1b40f6785dbaac2530fa5deb9f7c5
GadelhaTI <gadelha.ti@gmail.com>
2024-05-27 08:13:40 -0300

    feature: add unique constraint

--- src/main/java/br/eti/gadelha/maps/persistence/model/bndo/Manufacturer.java
+++ src/main/java/br/eti/gadelha/maps/persistence/model/bndo/Manufacturer.java
@@ -2,13 +2,15 @@ package br.eti.gadelha.maps.persistence.model.bndo;
 
 import br.eti.gadelha.maps.persistence.GenericAuditEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.hibernate.envers.Audited;
 
-@Audited @Entity @Data @AllArgsConstructor @NoArgsConstructor @EqualsAndHashCode(callSuper = true)
+@Audited @Entity @Data @AllArgsConstructor @NoArgsConstructor @Table(uniqueConstraints = @UniqueConstraint(columnNames = {"name"})) @EqualsAndHashCode(callSuper = true)
 public class Manufacturer extends GenericAuditEntity {
 
     private String name;
