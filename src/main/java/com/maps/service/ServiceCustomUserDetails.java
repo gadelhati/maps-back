7a1e7715cbf3dfc07ecb518aded14401a1d37fa6
GadelhaTI <gadelha.ti@gmail.com>
2025-03-19 22:09:49 -0300

    refactor: change custom user details.

--- src/main/java/com/maps/service/ServiceCustomUserDetails.java
+++ src/main/java/com/maps/service/ServiceCustomUserDetails.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -15,34 +16,35 @@ import java.util.*;
 
 @Service
 @RequiredArgsConstructor
-public class ServiceCustomUserDetails {
+public class ServiceCustomUserDetails implements UserDetailsService {
 
     private final RepositoryUser repositoryUser;
 
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = repositoryUser.findByUsername(username).orElse(null);
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = repositoryUser.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Resource not found"));
         return new org.springframework.security.core.userdetails.User(Objects.requireNonNull(user).getUsername(), user.getPassword(), getAuthorities(user.getRoles()));
     }
     private Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles) {
         return getGrantedAuthorities(getPrivileges(roles));
     }
-    private List<String> getPrivileges(Collection<Role> roles) {
-        List<String> privileges = new ArrayList<>();
-        List<Privilege> collection = new ArrayList<>();
-        for (Role role : roles) {
-            privileges.add(role.getId().toString());
-            collection.addAll(role.getPrivileges());
-        }
-        for (Privilege item : collection) {
-            privileges.add(item.getName());
-        }
-        return privileges;
-    }
-    private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
-        List<GrantedAuthority> authorities = new ArrayList<>();
+    private Set<GrantedAuthority> getGrantedAuthorities(Set<String> privileges) {
+        Set<GrantedAuthority> authorities = new HashSet<>();
         for (String privilege : privileges) {
             authorities.add(new SimpleGrantedAuthority(privilege));
         }
         return authorities;
+    }
+    private Set<String> getPrivileges(Collection<Role> roles) {
+        Set<String> permissions = new HashSet<>();
+        Set<Privilege> collection = new HashSet<>();
+        for (Role role : roles) {
+            permissions.add(role.getName());
+            collection.addAll(role.getPrivileges());
+        }
+        for (Privilege item : collection) {
+            permissions.add(item.getName());
+        }
+        return permissions;
     }
 }
