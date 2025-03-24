5ad42442ba289f99b70cc3c60b2e0ce6e5af1d9f
GadelhaTI <gadelha.ti@gmail.com>
2025-03-24 00:37:00 -0300

    chore: cleaning up

--- src/main/java/com/maps/ServletInitializer.java
+++ src/main/java/com/maps/ServletInitializer.java
@@ -15,5 +15,4 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(MapsApplication.class);
 	}
-
 }
